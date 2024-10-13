ee14c4d17e46be07b5e367e0c768643ac4991c8f
eeeleftheria <eleftheria.galiatsatou@gmail.com>
2024-10-13 17:47:45 +0300

    added comments in .h

--- graph.h
+++ graph.h
@@ -19,37 +19,57 @@ typedef struct edge* Edge;
 //η DestroyFunc ειναι δεικτης σε συναρτηση που καταστρεφει το value
 typedef void (*DestroyValueFunc)(Pointer value); 
 
+
 Graph graphCreate();
-void graphAdd(Graph graph, int user, HashTable hash_table); //δημιουργια και προσθηκη κομβου
-int graphSize(Graph graph); //επιστρεφει το μεγεθος του γραφου
-int graphGetUser(GraphNode graph_node);
+void graphAdd(Graph graph, int user, HashTable hash_table); //δημιουργια και προσθηκη κομβου με ονομα user
+void graphRemove(Graph graph, int user, HashTable hash_table); //αφαιρεση κορυφης με συγκεκριμενο user
 
+
+//ΣΥΝΑΡΤΗΣΕΙΣ DESTROY ΚΟΡΥΦΩΝ
 void graphDestroy(Graph graph, DestroyValueFunc func, HashTable table);
-void graphDestroyNode(Pointer graph_node);
+void graphDestroyNode(Pointer graph_node); //καταστρεφει εναν κομβο του γραφου, μαζι με ο,τι αυτος περιεχει
+//δηλαδη τις δυο λιστες με τις εισερχομενες και εξερχομενες ακμες και επειτα αποδεσμευει τη μνημη του γραφου
 
 
-///////////////////// ΔΕΝ ΕΧΩ ΥΛΟΠΟΙΗΣΕΙ
-void graphRemove(Graph graph, int user, HashTable hash_table);
+
+// Βοηθητικές συναρτήσεις
+int graphSize(Graph graph); //επιστρεφει το μεγεθος του γραφου
+int graphGetUser(GraphNode graph_node); //επιστρεφει τον user του συγκεκριμενου κομβου
 void graphDisplay(Graph graph);
 
-//////// ΣΥΝΑΡΤΗΣΕΙΣ ΔΙΑΧΕΙΡΙΣΗΣ ΑΚΜΩΝ
+
+///////////////////////////////////////////////
+//////// ΣΥΝΑΡΤΗΣΕΙΣ ΔΙΑΧΕΙΡΙΣΗΣ ΑΚΜΩΝ ////////
+///////////////////////////////////////////////
+
+//προσθηκη ακμης(συναλλαγης) απο τον χρηστη source_user προς τον dest_user
 void edgeAdd(Graph graph, int amount, char* date, int source_user, int dest_user, HashTable hash_table);
+
+//ευρεση συναλλαγης μεταξυ source_user - dest_user
 Edge edgeFind(Graph graph, int source_user, int dest_user, HashTable hash_table);
+
+//aφαιρεση ακμης μεταξυ source_user - dest_user
 void edgeRemove(Graph graph, int source_user, int dest_user, HashTable hash_table);
 
-///////////////////// ΔΕΝ ΕΧΩ ΥΛΟΠΟΙΗΣΕΙ
-void edgeValueDestroy();
 
-void edgeDestroyValueForIncoming(Pointer value);
+//ΣΥΝΑΡΤΗΣΕΙΣ DESTROY ΑΚΜΩΝ
+
+//καταστρεφει την λιστα incoming edges μιας κορυφης
+void incomingEdgesDestroy(GraphNode graph_node);
+
+//καταστρεφει την λιστα outgoing edges μιας κορυφης
+void outgoingEdgesDestroy(GraphNode graph_node);
+
+//αποδεσμευει την μνημη της ακμης value
 void edgeDestroyValueForOutgoing(Pointer value);
 
+//δεν κανει τιποτα, ο ορισμος της χρειαζεται για να περαστει ως παραμετρος στην list destroy
+//η ακμη πρεπει να αποδεσμευτει μονο μια φορα, αφου υπαρχει και στις δυο λιστες.
+//οποτε αυτο γινεται στην απο πανω συναρτηση για τις outgoing 
+void edgeDestroyValueForIncoming(Pointer value);
 
-void incomingEdgeDestroyValue(Pointer value);
-void outgoingEdgesDestroyValue(Pointer value);
 
 
-void incomingEdgesDestroy(GraphNode graph_node);
-void outgoingEdgesDestroy(GraphNode graph_node);
 
 
 
--- hash.h
+++ hash.h
@@ -13,20 +13,33 @@ typedef int (*CompareFunc)(Pointer a, Pointer b);
 int hashFunc(int user, int M); // hush function που παιρνει ενα κλειδι και το αντιστοιχιζει σε ενα index 
 
 HashTable hashCreate(int size);
-void hashAdd(HashTable hash_table, int key, Pointer value);
-int hashSize(HashTable hash_table);
+
+//προσθετει μια κορυφη στο hash table
+void hashAdd(HashTable hash_table, int user, Pointer value);
 
 //αφαιρουμε εναν δεδομενο user απο το hash table
 void hashRemove(HashTable hash_table, int user, DestroyValueFunc func);
 
+//επιστρεφει το size του hash
+int hashSize(HashTable hash_table);
+
+
 //για εναν δεδομενο user θελουμε να επιστρεψουμε τα στοιχεια του
 Pointer hashFindGraphNodeWithKey(HashTable hash_table, int user);
+
+//για εναν δεδομενο user επιστρεφουμε το list node στο οποιο ανηκει 
 Pointer hashFindListNodeWithKey(HashTable hash_table, int user);
+
+//για εναν δεδομενο graph node επιστρεφουμε τον list node στον οποιο ανηκει
 Pointer hashFindListNodeWithValue(HashTable hash_table, Pointer value);
 
 
+//ΣΥΝΑΡΤΗΣΕΙΣ DESTROY 
 
 void hashDestroy(HashTable hash_table);
+
+//δεν κανει τιποτα, ο ορισμος της χρειαζεται για την list destroy
+//το value ειναι graph node και καταστρεφεται απο την graph destroy
 void hashDestroyValue(Pointer value);
 
 
